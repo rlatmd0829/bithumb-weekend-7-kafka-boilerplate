08bd85745d3ecc5151a8c8b997f74a01a9fe96f6
parkjungkwan <pakjkwan@gmail.com>
2021-09-01 20:24:29 +0900

    first commit

---
+++ src/main/java/net/zerotodev/api/quiz/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package net.zerotodev.api.quiz;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component @RequiredArgsConstructor
+public class KafkaConsumer {
+    @KafkaListener(topics="sample", groupId = "myGroup")
+    public void consume(String message) throws IOException {
+        System.out.println("Consumer Message is "+ message);
+    }
+}
---
+++ src/main/java/net/zerotodev/api/quiz/KafkaProducer.java
@@ -0,0 +1,18 @@
+package net.zerotodev.api.quiz;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private static final String TOPIC = "sample";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        System.out.println(" Producer Message " + message );
+    }
+}
